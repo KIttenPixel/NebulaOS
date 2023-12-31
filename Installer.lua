499da542bffd5924d467f7836830532186cf4cd8
KittenPixel-cell <gamergirl@hedlin.com>
2023-12-30 21:24:51 -0800

    Added files.cfg

---
+++ Installer.lua
@@ -0,0 +1,125 @@
+local installerConfigs = "Installer/"
+
+-- Checking for required components
+local function getComponentAddress(name)
+	return component.list(name)() or error("Required " .. name .. " component is missing")
+end
+
+local repositoryURL = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/"
+
+local internetAddress, GPUAddress = 
+	getComponentAddress("internet"),
+	getComponentAddress("gpu")
+
+local function filesystemPath(path)
+	return path:match("^(.+%/).") or ""
+end
+
+local function filesystemName(path)
+	return path:match("%/?([^%/]+%/?)$")
+end
+
+local function filesystemHideExtension(path)
+	return path:match("(.+)%..+") or path
+end
+
+local function rawRequest(url, chunkHandler)
+	local internetHandle, reason = component.invoke(internetAddress, "request", repositoryURL .. url:gsub("([^%w%-%_%.%~])", function(char)
+		return string.format("%%%02X", string.byte(char))
+	end))
+
+	if internetHandle then
+		local chunk, reason
+		while true do
+			chunk, reason = internetHandle.read(math.huge)	
+			
+			if chunk then
+				chunkHandler(chunk)
+			else
+				if reason then
+					error("Internet request failed: " .. tostring(reason))
+				end
+
+				break
+			end
+		end
+
+		internetHandle.close()
+	else
+		error("Connection failed: " .. url)
+	end
+end
+
+local function request(url)
+	local data = ""
+	
+	rawRequest(url, function(chunk)
+		data = data .. chunk
+	end)
+
+	return data
+end
+
+local function download(url, path)
+	selectedFilesystemProxy.makeDirectory(filesystemPath(path))
+
+	local fileHandle, reason = selectedFilesystemProxy.open(path, "wb")
+	if fileHandle then	
+		rawRequest(url, function(chunk)
+			selectedFilesystemProxy.write(fileHandle, chunk)
+		end)
+
+		selectedFilesystemProxy.close(fileHandle)
+	else
+		error("File opening failed: " .. tostring(reason))
+	end
+end
+
+local function deserialize(text)
+	local result, reason = load("return " .. text, "=string")
+	if result then
+		return result()
+	else
+		error(reason)
+	end
+end
+
+component.invoke(GPUAddress, "setBackground", 0xE1E1E1)
+component.invoke(GPUAddress, "fill", 1, 1, screenWidth, screenHeight, " ")
+
+do
+	local function warning(text)
+		centrizedText(title(), 0x878787, text)
+
+		local signal
+		repeat
+			signal = computer.pullSignal()
+		until signal == "key_down" or signal == "touch"
+
+		computer.shutdown()
+	end
+
+	if component.invoke(GPUAddress, "getDepth") ~= 8 then
+		warning("Tier 3 GPU and screen are required")
+	end
+
+	if computer.totalMemory() < 1024 * 1024 * 2 then
+		warning("At least 2x Tier 3.5 RAM modules are required")
+	end
+
+	-- Searching for appropriate temporary filesystem for storing libraries, images, etc
+	for address in component.list("filesystem") do
+		local proxy = component.proxy(address)
+		if proxy.spaceTotal() >= 2 * 1024 * 1024 then
+			temporaryFilesystemProxy, selectedFilesystemProxy = proxy, proxy
+			break
+		end
+	end
+
+	-- If there's no suitable HDDs found - then meow
+	if not temporaryFilesystemProxy then
+		warning("At least Tier 2 HDD is required")
+	end
+end
+
+local files = deserialize(request(installerURL .. "Files.cfg"))
